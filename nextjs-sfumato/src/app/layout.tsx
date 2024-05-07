45424474c400656524f4ed1c704ba16aa7870dfb
RJ <richard.nader@fynydd.com>
2024-05-07 17:11:09 -0400

    Fixed nextjs build script.

--- nextjs-sfumato/src/app/layout.tsx
+++ nextjs-sfumato/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../assets/stylesheets/application.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
