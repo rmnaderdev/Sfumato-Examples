63f3c1a56591e6eb7c28c2971c7650188a3558de
RJ <richard.nader@fynydd.com>
2024-05-07 16:55:52 -0400

    Added tailwind.config.ts file for IDE intellisense support. Added nextjs project.

---
+++ nextjs-sfumato/tailwind.config.ts
@@ -0,0 +1,8 @@
+
+// Having the `tailwind.config.ts` file is intentional. It tells the ide how to handle the sfumato classes in the intellisense.
+
+const config = {
+  content: [],
+  plugins: [],
+};
+export default config;
--- vue3-sfumato/README.md
+++ vue3-sfumato/README.md
@@ -32,3 +32,5 @@ yarn sfumato
 ```sh
 yarn build
 ```
+
+Having the `tailwind.config.ts` file is intentional. It tells the ide how to handle the sfumato classes in the intellisense.
